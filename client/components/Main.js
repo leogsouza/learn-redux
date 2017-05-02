95a2febdeecee8d03975f2c2fd85a1cc02271a91
Leonardo Souza <leogsouza@gmail.com>
2017-05-02 16:34:22 -0300

    Creating our Single and PhotoGrid components

--- client/components/Main.js
+++ client/components/Main.js
@@ -11,6 +11,8 @@ class Main extends React.Component {
         <h1>
           <Link to="/">Reduxstagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
+
       </div>
     )
   }
