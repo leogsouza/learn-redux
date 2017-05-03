5ed5e6d07fbc69be29809a288f175d790a1017fa
Leonardo Souza <leogsouza@gmail.com>
2017-05-03 16:19:50 -0300

    Displaying Single Photo Component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
-export default () => <div className="single-photo">
-  I am a Single
-</div>
+class Single extends React.Component {
+  // index of the post
+  render() {
+    const props = this.props;
+    const i = props.posts.findIndex((post) => post.code === props.params.postId)
+    const post = this.props.posts[i];
+    console.log(post);
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...props} />
+        <Comments />
+      </div>
+    )
+  }
+}
+
+export default Single;
