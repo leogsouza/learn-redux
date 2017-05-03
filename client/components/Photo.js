bb9ad0b2018bb3a540ad0251e2c3293bde53de54
Leonardo Souza <leogsouza@gmail.com>
2017-05-03 15:29:14 -0300

    Updating state with reducers

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -22,7 +22,7 @@ const Photo = (props) => {
       <figcaption>
         <p>{post.caption}</p>
         <div className="control-buttons">
-          <button className="likes">&hearts; {post.likes}</button>
+          <button className="likes" onClick={props.increment.bind(null, i)}>&hearts; {post.likes}</button>
           <Link className="button" to={`/view/${post.code}`}>
             <span className="comment-count">
               <span className="speech-bubble"></span>
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -4,8 +4,19 @@
 // 2. copy of current store
 
 function posts(state = [], action) {
-  console.log(state, action );
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+    const i = action.index;
+      return [
+        ...state.slice(0,i), // before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), // after the one we are updating
+
+      ]
+      break;
+    default:
+      return state;
+  }
 }
 
 export default posts;
